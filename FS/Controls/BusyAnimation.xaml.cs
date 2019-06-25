6239b7f67c09c563ba083d03d93c0eab052f483c
Peter Vietense <peter.vietense@infact-wws.de>
2019-06-25 10:18:38 +0200

    busy animation fixes

--- FS/Controls/BusyAnimation.xaml.cs
+++ FS/Controls/BusyAnimation.xaml.cs
@@ -1,7 +1,21 @@
-﻿namespace FS
+﻿using System.Windows;
+
+namespace FS
 {
     public partial class BusyAnimation
     {
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
+            "Text",
+            typeof(string),
+            typeof(BusyAnimation),
+            new PropertyMetadata(default(string)));
+
         public BusyAnimation()
         {
             InitializeComponent();
